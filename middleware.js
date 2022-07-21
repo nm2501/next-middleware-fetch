f38b0ceb0616ccb2e84d284793ab47ea32a87ec5
Neil McMillan <nm2501@gmail.com>
2022-07-21 13:58:48 +0100

    Minimal example of problem with fetch() from edge middleware to an edge function yielding no result.

---
+++ middleware.js
@@ -0,0 +1,25 @@
+
+import { NextResponse } from "next/server";
+
+export async function middleware(req) {
+  const url = req.nextUrl;
+  const { pathname } = url;
+
+  if (pathname.startsWith("/_next")) {
+    return NextResponse.next();
+  }
+
+  const fetchUrl = url.origin + "/api/hello"
+  console.log('fetchUrl: ', fetchUrl);
+  const fetchResult = await fetch(fetchUrl, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  console.log("result: ", typeof fetchResult, Object.keys(fetchResult), fetchResult);
+  const payload = fetchResult.json();
+  console.log("payload: ", payload, Object.keys(payload));
+
+  return NextResponse.next();
+}
